513da6a013a9d1271eba932ba90385cc0d315f03
Kambata <kamen.e.kanev@gmail.com>
2015-05-04 14:15:10 +0300

    Remove it to start over, too much not needed things.

--- week3/1-microblog/forum/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-
-  root 'posts#index'
-  get '/new', to: 'posts#new'
-  post '/new', to: 'posts#create'
-  get '/:id', to: 'posts#show'
-  delete '/:id', to: 'posts#destroy'
-
-end
